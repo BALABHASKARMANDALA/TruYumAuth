db33bfbca2b13bfd949beb7cd0b7fe1df58da8c5
balabhaskar <balabhaskarmandala@gmail.com>
2020-12-08 13:25:33 +0530

    first commit

---
+++ Repository/UserDetailsRepository.cs
@@ -0,0 +1,22 @@
+﻿using AuthService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthService.Repository
+{
+    public class UserDetailsRepository: IUserDetailsRepository
+    {
+        public static List<UserDetails> user = new List<UserDetails>()
+        {
+            new UserDetails{Userid=1,Username="abc",Password="abc@123"},
+            new UserDetails{Userid=2,Username="xyz",Password="xyz@123"}
+        };
+
+        public UserDetails GetUserDetails(UserDetails valuser)
+        {
+            return user.FirstOrDefault(c => c.Username == valuser.Username && c.Password == valuser.Password);
+        }
+    }
+}
